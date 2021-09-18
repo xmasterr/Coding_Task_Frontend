ea0d34f02ac5585f4f349b0317c03b9a7b3fae20
xmasterr <33792788+xmasterr@users.noreply.github.com>
2021-09-18 14:17:34 +0200

    "first try"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {HttpClient} from '@angular/common/http';
+
+
+declare var myTest: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'AngularCodingTask';
+  text = '';
+  checked = true;
+  resultMaP = {};
+
+  constructor( private http: HttpClient) {}
+
+  analyzer() {
+    //online
+    if (this.checked){
+    this.http.get<any>('/api/analyzer?text='+this.text).subscribe(data => this.resultMaP = data);
+
+    //offline
+    }else {
+      //TODO:CALL SCRIPT
+ //     textAnalyzerScript.analyse();
+myTest();
+    }
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {FormsModule} from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
